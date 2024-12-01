87019db799968cf0450e452981465557218b93c4
Habiba Amr <habibaamr350@gmail.com>
2024-12-01 16:37:37 +0200

    Add apiBaseUrlTo property file

--- TestAutomationTask/src/main/java/api/endpoints/Routes.java
+++ TestAutomationTask/src/main/java/api/endpoints/Routes.java
@@ -1,8 +1,11 @@
 package api.endpoints;
 
+import java.util.Properties;
+
+import utils.PropertiesReader;
+
 public class Routes {
-	
-	public static String base_url="https://jsonplaceholder.typicode.com";
+	public static String base_url=PropertiesReader.getProperties().getProperty("base_url_api");
 	
 	
 	public static String post_url=base_url+"/posts";
